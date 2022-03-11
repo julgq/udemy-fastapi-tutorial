07c29be9a0fc29f9d663609cd39e4ce8f60ed5cc
Julio Gtz <jc.gutierrezq@gmail.com>
2022-03-11 16:09:35 -0700

    password

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,9 +1,13 @@
+from http.client import HTTPException
 from venv import create
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional
 import models
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from fastapi.security import OAuth2PasswordRequestForm
 
 class CreateUser(BaseModel):
     username: str
@@ -14,13 +18,46 @@ class CreateUser(BaseModel):
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+# funcion para para encriptar un password
 def get_password_hash(password):
     return bcrypt_context.hash(password)
 
+# funcion para verificar el texto plano de password contra el hashed almacenado.
+def verify_password(plain_password, hashed_password):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+# funcion para hacerla la verificación de usuario y contraseña
+def authenticate_user(username:str, password: str, db):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+# funcion para solicitar usuer y password y validar.
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return "User Validated"
+
+
 @app.post("/create/user")
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
@@ -28,5 +65,8 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.last_name = create_user.last_name
     create_user_model.hashed_password = get_password_hash(create_user.password)
     create_user_model.is_active = True
+
+    db.add(create_user_model)
+    db.commit()
 
     return create_user_model
