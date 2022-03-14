f7e4b8f3b23ce114c9dbad3cf237e0bd624d02ec
Julio Gtz <jc.gutierrezq@gmail.com>
2022-03-14 15:34:26 -0700

    token

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,13 +1,17 @@
-from http.client import HTTPException
-from venv import create
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel
+import time
+from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Optional
+from pydantic import BaseModel, Field
 import models
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import date, datetime, timedelta
+from jose import jwt, JWTError
+
+SECRET_KEY = "4PYkCouLcbtSlM306lMrxQRcqHZGDt9k"
+ALGORITHM = "HS256"
 
 class CreateUser(BaseModel):
     username: str
@@ -19,6 +23,8 @@ class CreateUser(BaseModel):
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -47,13 +53,54 @@ def authenticate_user(username:str, password: str, db):
         return False
     return user
 
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = date.utcnow() + timedelta(minutes=15)
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: str = Depends(oauth2_bearer)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise get_user_exception()
+    except JWTError:
+        raise get_user_exception()
+
 # funcion para solicitar usuer y password y validar.
 @app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return "User Validated"
+        raise token_exception()
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
+
+    return {"token": token}
+
+
+# Exceptions:
+def get_user_exception():
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return credentials_exception
+
+def token_exception():
+    token_exception_response = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return token_exception_response
+
 
 
 @app.post("/create/user")
