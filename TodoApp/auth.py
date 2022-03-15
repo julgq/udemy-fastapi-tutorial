fd05ef4776b875bb0342da338680ac11d1e39814
Julio Gtz <jc.gutierrezq@gmail.com>
2022-03-15 16:48:07 -0700

    postgresql and config token request

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,17 +1,18 @@
-import time
 from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel
 from typing import Optional
-from pydantic import BaseModel, Field
 import models
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from jose import jwt, JWTError
 
-SECRET_KEY = "4PYkCouLcbtSlM306lMrxQRcqHZGDt9k"
+
+SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
 ALGORITHM = "HS256"
+
 
 class CreateUser(BaseModel):
     username: str
@@ -20,13 +21,16 @@ class CreateUser(BaseModel):
     last_name: str
     password: str
 
+
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
+
 app = FastAPI()
+
 
 def get_db():
     try:
@@ -35,17 +39,19 @@ def get_db():
     finally:
         db.close()
 
-# funcion para para encriptar un password
+
 def get_password_hash(password):
     return bcrypt_context.hash(password)
 
-# funcion para verificar el texto plano de password contra el hashed almacenado.
+
 def verify_password(plain_password, hashed_password):
     return bcrypt_context.verify(plain_password, hashed_password)
 
-# funcion para hacerla la verificación de usuario y contraseña
-def authenticate_user(username:str, password: str, db):
-    user = db.query(models.Users).filter(models.Users.username == username).first()
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users)\
+        .filter(models.Users.username == username)\
+        .first()
 
     if not user:
         return False
@@ -53,14 +59,18 @@ def authenticate_user(username:str, password: str, db):
         return False
     return user
 
-def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+
+def create_access_token(username: str, user_id: int,
+                        expires_delta: Optional[timedelta] = None):
+
     encode = {"sub": username, "id": user_id}
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = date.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=15)
     encode.update({"exp": expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
 async def get_current_user(token: str = Depends(oauth2_bearer)):
     try:
@@ -69,37 +79,9 @@ async def get_current_user(token: str = Depends(oauth2_bearer)):
         user_id: int = payload.get("id")
         if username is None or user_id is None:
             raise get_user_exception()
+        return {"username": username, "id": user_id}
     except JWTError:
         raise get_user_exception()
-
-# funcion para solicitar usuer y password y validar.
-@app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(form_data.username, form_data.password, db)
-    if not user:
-        raise token_exception()
-    token_expires = timedelta(minutes=20)
-    token = create_access_token(user.username, user.id, expires_delta=token_expires)
-
-    return {"token": token}
-
-
-# Exceptions:
-def get_user_exception():
-    credentials_exception = HTTPException(
-        status_code = status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-    return credentials_exception
-
-def token_exception():
-    token_exception_response = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Incorrect username or password",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    return token_exception_response
 
 
 @app.post("/create/user")
@@ -109,10 +91,45 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
     create_user_model.username = create_user.username
     create_user_model.first_name = create_user.first_name
     create_user_model.last_name = create_user.last_name
-    create_user_model.hashed_password = get_password_hash(create_user.password)
+
+    hash_password = get_password_hash(create_user.password)
+
+    create_user_model.hashed_password = hash_password
     create_user_model.is_active = True
 
     db.add(create_user_model)
     db.commit()
 
-    return create_user_model
+
+# login and return the token.
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise token_exception()
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username,
+                                user.id,
+                                expires_delta=token_expires)
+    return {"token": token}
+
+
+#Exceptions
+def get_user_exception():
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return credentials_exception
+
+
+def token_exception():
+    token_exception_response = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return token_exception_response
+
--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# conexión con sqlite
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# conexión con postgres
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/postgres"
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
+    #solo para sqlite
+    #SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
+   
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
