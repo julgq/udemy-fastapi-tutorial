e04e41d1820d13565016289627a8be368a0c3c49
Julio Gtz <jc.gutierrezq@gmail.com>
2022-03-15 17:56:34 -0700

    mysql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# conexión con sqlite
+# sqlite connection
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-# conexión con postgres
+# postgres connection
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/postgres"
+
+# mysql connection
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
     #solo para sqlite
